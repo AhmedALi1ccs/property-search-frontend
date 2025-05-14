0b166994b4b16fb08911e26a61146ae107a13df3
AhmedALi <ahmedali27428@gmail.com>
2025-05-14 17:24:28 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,20 +2,17 @@
 import React from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './styles/App.css';
-
-
 import HomePage from './pages/HomePage';
 import SearchPage from './pages/SearchPage';
 import ImportPage from './pages/ImportPage';
 import ExportPage from './pages/ExportPage';
-
 // Import components
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/property-search-frontend">
       <div className="app">
         <Navbar />
         <main className="main-content">
