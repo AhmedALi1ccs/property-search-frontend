5a228e3a0897c7df755832c80723d63b0f3c04bf
Ahmedali270402 <ahmed@goccs.net>
2025-06-17 05:29:39 +0300

    live

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 // src/config.js
 const config = {
   // Use the deployed API URL to avoid CORS issues
-  //API_URL: 'https://mailing-database-app.uc.r.appspot.com/api/v1'
-  
+  API_URL: 'https://mailing-database-app.uc.r.appspot.com/api/v1'
+}
   // If you want to use localhost, uncomment this and make sure CORS is configured
-  API_URL: process.env.NODE_ENV === 'production' 
-    ? 'https://mailing-database-app.uc.r.appspot.com/api/v1'
-    : 'http://localhost:3000/api/v1'
-};
+//   API_URL: process.env.NODE_ENV === 'production' 
+//     ? 'https://mailing-database-app.uc.r.appspot.com/api/v1'
+//     : 'http://localhost:3000/api/v1'
+// };
 
 export default config;
